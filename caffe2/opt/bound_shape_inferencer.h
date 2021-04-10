52e65a02b8fe0ff4cbbebda4e30901cf99eb4197
automation <automation@mygamma.io>
2021-04-10 11:59:55 +0530

    commitMessage : Add shape inference for SparseLengthsSumSparseLookup

--- caffe2/opt/bound_shape_inferencer.h
+++ caffe2/opt/bound_shape_inferencer.h
@@ -135,9 +135,6 @@ class TORCH_API BoundShapeInferencer : public BoundShapeInferencerBase {
   void InferUnPackRecords(const OperatorDef& op);
   void InferTile(const OperatorDef& op);
   void InferSparseLengthsSumSparseLookup(const OperatorDef& op);
-  void InferSoftmax(const OperatorDef& op);
-  void InferLpNorm(const OperatorDef& op);
-  void InferTranspose(const OperatorDef& op);
 
   // Standard shape/type inference using op schema registered shape inference
   // function
