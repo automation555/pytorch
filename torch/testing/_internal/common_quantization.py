94c3359bea073039a80d0586ccca062278d92e55
automation <automation@mygamma.io>
2021-04-10 22:10:16 +0530

    commitMessage : [quant] Add another keyword to the 'prepare_custom_config_dict' to be the same as in FX

--- torch/testing/_internal/common_quantization.py
+++ torch/testing/_internal/common_quantization.py
@@ -6,19 +6,19 @@ import torch
 import torch.nn as nn
 import torch.nn.quantized as nnq
 import torch.nn.quantized.dynamic as nnqd
-from torch.nn.intrinsic import _FusedModule
 import torch.distributed as dist
 
 from torch.testing._internal.common_utils import TestCase
 from torch.quantization import QuantWrapper, QuantStub, DeQuantStub, \
     default_qconfig, default_dynamic_qconfig, default_per_channel_qconfig, QConfig, default_observer, default_weight_observer, \
-    propagate_qconfig_, convert, get_default_qconfig, quantize_dynamic_jit, quantize_jit, float_qparams_weight_only_qconfig, \
+    propagate_qconfig_, convert, get_default_qconfig, quantize_dynamic_jit, quantize_jit, float_qparams_dynamic_qconfig, \
     get_default_qat_qconfig, PerChannelMinMaxObserver, default_dynamic_quant_observer, QConfigDynamic, QuantType
 from torch.quantization.quantization_mappings import (
     get_default_dynamic_quant_module_mappings,
     get_default_qconfig_propagation_list,
     get_default_qat_module_mappings,
 )
+from torch.quantization.quant_type import quant_type_to_str
 
 try:
     # graph mode quantization based on fx
@@ -27,9 +27,6 @@ try:
         prepare_qat_fx,
         convert_fx,
     )
-    from torch.quantization.ns.ns_types import NSSingleResultValuesType, NSSubgraph
-    from torch.fx.graph import Node
-    from torch.fx import GraphModule
     HAS_FX = True
 except ImportError:
     HAS_FX = False
@@ -43,7 +40,6 @@ import os
 import unittest
 import numpy as np
 from torch.testing import FileCheck
-from typing import Callable, Tuple, Dict, Any
 
 class NodeSpec:
     ''' Used for checking GraphModule Node
@@ -361,18 +357,18 @@ class QuantizationTestCase(TestCase):
                     submodule_name_count += 1
             return submodule_name_count == 0
 
+        quant_type = getattr(getattr(module, 'qconfig', None), 'quant_type', None)
+        quant_type_str = quant_type_to_str(quant_type)
         if hasattr(module, 'qconfig') and module.qconfig is not None and \
            ((is_leaf_module(module) and not isinstance(module, torch.nn.Sequential)
             and type(module) in propagate_qconfig_list) or
-           type(module) in float_to_observed_module_class_mapping.keys()) and \
-           not isinstance(module, torch.quantization.DeQuantStub):
+           type(module) in float_to_observed_module_class_mapping.get(quant_type_str, {}).keys()):
             self.assertTrue(hasattr(module, 'activation_post_process'),
                             'module: ' + str(type(module)) + ' do not have observer')
         # we don't need to check observers for child modules of the
         # qat modules
         if type(module) not in get_default_qat_module_mappings().values() and \
-           type(module) not in float_to_observed_module_class_mapping.values() and \
-           not isinstance(module, _FusedModule):
+           type(module) not in float_to_observed_module_class_mapping.get(quant_type_str, {}).values():
             for child in module.children():
                 self.checkObservers(child, propagate_qconfig_list, prepare_custom_config_dict)
 
@@ -501,20 +497,20 @@ class QuantizationTestCase(TestCase):
             print('input graph:', model.graph)
         models = {}
         outputs = {}
-        for debug in [True, False]:
+        for d in [True, False]:
             if dynamic:
-                models[debug] = quantize_dynamic_jit(model, qconfig_dict, debug=debug)
+                models[d] = quantize_dynamic_jit(model, qconfig_dict, debug=d)
                 # make sure it runs
-                outputs[debug] = models[debug](inputs)
+                outputs[d] = models[d](inputs)
             else:
                 # module under test can contain in-place ops, and we depend on
                 # input data staying constant for comparisons
                 inputs_copy = copy.deepcopy(inputs)
-                models[debug] = quantize_jit(
+                models[d] = quantize_jit(
                     model, qconfig_dict, test_only_eval_fn, [inputs_copy], inplace=False,
-                    debug=debug)
+                    debug=d)
                 # make sure it runs
-                outputs[debug] = models[debug](*inputs[0])
+                outputs[d] = models[d](*inputs[0])
 
         if debug:
             print('debug graph:', models[True].graph)
@@ -608,126 +604,13 @@ class QuantizationTestCase(TestCase):
         return str_to_print
 
     if HAS_FX:
-
-        def assert_types_for_matched_subgraph_pairs(
-            self,
-            matched_subgraph_pairs: Dict[str, Tuple[NSSubgraph, NSSubgraph]],
-            expected_types: Dict[str, Tuple[Tuple[Callable, Callable], Tuple[Callable, Callable]]],
-            gm_a: GraphModule,
-            gm_b: GraphModule,
-        ) -> None:
-            """
-            Verifies that the types specified in expected_types match
-            the underlying objects pointed to by the nodes in matched_subgraph_pairs.
-
-            An example successful test case:
-
-              matched_subgraph_pairs = {'x0': (graph_a_conv_0_node, graph_b_conv_0_node)}
-              expected_types = {'x0': (nn.Conv2d, nnq.Conv2d)}
-
-            The function tests for key equivalence, and verifies types with
-            instance checks.
-            """
-
-            def _get_underlying_op_type(node: Node, gm: GraphModule) -> Callable:
-                if node.op == 'call_module':
-                    mod = getattr(gm, node.target)
-                    return type(mod)
-                else:
-                    assert node.op == 'call_function'
-                    return node.target
-
-            self.assertTrue(
-                len(matched_subgraph_pairs) == len(expected_types),
-                'Expected length of results to match, but got %d and %d' %
-                (len(matched_subgraph_pairs), len(expected_types))
-            )
-            for k, v in expected_types.items():
-                expected_types_a, expected_types_b = v
-                exp_type_start_a, exp_type_end_a = expected_types_a
-                exp_type_start_b, exp_type_end_b = expected_types_b
-                subgraph_a, subgraph_b = matched_subgraph_pairs[k]
-
-                act_type_start_a = _get_underlying_op_type(subgraph_a.start_node, gm_a)
-                act_type_start_b = _get_underlying_op_type(subgraph_b.start_node, gm_b)
-                act_type_end_a = _get_underlying_op_type(subgraph_a.end_node, gm_a)
-                act_type_end_b = _get_underlying_op_type(subgraph_b.end_node, gm_b)
-                types_match = (exp_type_start_a is act_type_start_a) and \
-                    (exp_type_end_a is act_type_end_a) and \
-                    (exp_type_start_b is act_type_start_b) and \
-                    (exp_type_end_b is act_type_end_b)
-                self.assertTrue(
-                    types_match,
-                    'Type mismatch at %s: expected %s, got %s' %
-                    (k, (exp_type_start_a, exp_type_end_a, exp_type_start_b, exp_type_end_b),
-                        (act_type_start_a, act_type_end_a, act_type_start_b, act_type_end_b))
-                )
-
-        def assert_ns_compare_dict_valid(
-            self,
-            act_compare_dict: Dict[str, Dict[str, Dict[str, Any]]],
-        ) -> None:
-            """
-            Verifies that the act_compare_dict (output of Numeric Suite APIs) is valid:
-            1. for each layer, results are recorded for two models
-            2. number of seen tensors match
-            3. shapes of each pair of seen tensors match
-            """
-            for layer_name, result_type_to_data in act_compare_dict.items():
-                for result_type, layer_data in result_type_to_data.items():
-                    self.assertTrue(
-                        len(layer_data) == 2,
-                        f"Layer {layer_name} does not have exactly two model results.")
-                    model_name_0, model_name_1 = layer_data.keys()
-                    for res_idx in range(len(layer_data[model_name_0])):
-                        layer_data_0 = layer_data[model_name_0][res_idx]
-                        layer_data_1 = layer_data[model_name_1][res_idx]
-                        self.assertTrue(
-                            layer_data_0['type'] == layer_data_0['type'],
-                            f"Layer {layer_name}, {model_name_0} and {model_name_1} do not have the same type.")
-                        self.assertTrue(
-                            len(layer_data_0['values']) ==
-                            len(layer_data_1['values']),
-                            f"Layer {layer_name}, {model_name_0} and {model_name_1} do not have the same number of seen Tensors.")
-                        for idx in range(len(layer_data_0['values'])):
-                            values_0 = layer_data_0['values'][idx]
-                            values_1 = layer_data_1['values'][idx]
-                            if isinstance(values_0, torch.Tensor):
-                                self.assertTrue(
-                                    values_0.shape == values_1.shape,
-                                    f"Layer {layer_name}, {model_name_0} and {model_name_1} have a shape mismatch at idx {idx}.")
-                            else:
-                                assert isinstance(values_0, tuple), \
-                                    f"unhandled type {type(values_0)}"
-                                assert len(values_0) == 2
-                                assert len(values_0[1]) == 2
-                                assert values_0[0].shape == values_1[0].shape
-                                assert values_0[1][0].shape == values_1[1][0].shape
-                                assert values_0[1][1].shape == values_1[1][1].shape
-
-                        # verify that ref_node_name is valid
-                        ref_node_name_0 = layer_data_0['ref_node_name']
-                        ref_node_name_1 = layer_data_1['ref_node_name']
-                        prev_node_name_0 = layer_data_0['prev_node_name']
-                        prev_node_name_1 = layer_data_1['prev_node_name']
-                        if layer_data_0['type'] == NSSingleResultValuesType.NODE_OUTPUT.value:
-                            self.assertTrue(ref_node_name_0 == prev_node_name_0)
-                            self.assertTrue(ref_node_name_1 == prev_node_name_1)
-                        elif layer_data_0['type'] == NSSingleResultValuesType.NODE_INPUT.value:
-                            self.assertTrue(ref_node_name_0 != prev_node_name_0)
-                            self.assertTrue(ref_node_name_1 != prev_node_name_1)
-
         def checkGraphModeFxOp(self, model, inputs, quant_type,
                                expected_node=None,
                                expected_node_occurrence=None,
                                expected_node_list=None,
-                               is_reference=False,
+                               debug=False,
                                print_debug_info=False,
-                               custom_qconfig_dict=None,
-                               prepare_expected_node=None,
-                               prepare_expected_node_occurrence=None,
-                               prepare_expected_node_list=None,
-                               prepare_custom_config_dict=None):
+                               custom_qconfig=None):
             """ Quantizes model with graph mode quantization on fx and check if the
                 quantized model contains the quantized_node
 
@@ -746,14 +629,6 @@ class QuantizationTestCase(TestCase):
                                 NodeSpec.call_module(nnq.Conv2d),
                                 NodeSpec.call_function(F.hardtanh_),
                                 NodeSpec.call_method('dequantize')]
-                    is_reference: if True, enables reference mode
-                    print_debug_info: if True, prints debug info
-                    custom_qconfig_dict: overrides default qconfig_dict
-                    prepare_expected_node: same as expected_node, but for prepare
-                    prepare_expected_node_occurrence: same as
-                        expected_node_occurrence, but for prepare
-                    prepare_expected_node_list: same as expected_node_list, but
-                        for prepare
             """
             # TODO: make img_data a single example instead of a list
             if type(inputs) == list:
@@ -769,47 +644,36 @@ class QuantizationTestCase(TestCase):
                 qconfig = default_dynamic_qconfig
                 model.eval()
 
+            # overwrite qconfig with custom_qconfig
+            if custom_qconfig is not None:
+                qconfig = custom_qconfig
+
             if quant_type == QuantType.QAT:
                 prepare = prepare_qat_fx
             else:
                 prepare = prepare_fx
 
-            qconfig_dict = {"": qconfig}
-            # overwrite qconfig_dict with custom_qconfig_dict
-            if custom_qconfig_dict is not None:
-                qconfig_dict = custom_qconfig_dict
-            prepared = prepare(
-                model, qconfig_dict,
-                prepare_custom_config_dict=prepare_custom_config_dict)
+            qconfig_dict = {'': qconfig}
+            prepared = prepare(model, qconfig_dict)
             if not quant_type == QuantType.DYNAMIC:
                 prepared(*inputs)
-
-            if print_debug_info:
-                print()
-                print('quant type:\n', quant_type)
-                print('original model:\n', model)
-                print()
-                print('prepared model:\n', prepared)
-
-            self.checkGraphModuleNodes(
-                prepared, prepare_expected_node,
-                prepare_expected_node_occurrence, prepare_expected_node_list)
-
             prepared_copy = copy.deepcopy(prepared)
             qgraph = convert_fx(prepared)
-            qgraph_reference = convert_fx(prepared_copy, is_reference=True)
+            qgraph_debug = convert_fx(prepared_copy, debug=True)
             result = qgraph(*inputs)
-            result_reference = qgraph_reference(*inputs)
+            result_debug = qgraph_debug(*inputs)
 
-            qgraph_to_check = qgraph_reference if is_reference else qgraph
+            qgraph_to_check = qgraph_debug if debug else qgraph
             if print_debug_info:
                 print()
-                print('quantized model:\n', qgraph_to_check)
+                print('quant type:', quant_type)
+                print('original model:', model)
+                print()
+                print('quantized model:', qgraph_to_check)
                 self.printGraphModule(qgraph_to_check)
                 print()
             self.checkGraphModuleNodes(
                 qgraph_to_check, expected_node, expected_node_occurrence, expected_node_list)
-            return result
 
 
     def checkEmbeddingSerialization(self, qemb, num_embeddings, embedding_dim, indices, offsets,
@@ -878,8 +742,8 @@ class QuantizationTestCase(TestCase):
         self.assertTrue(expected_name in str(q_embeddingbag))
 
 
-# Below are a series of toy models to use in testing quantization
-
+# Below are a series of neural net models to use in testing quantization
+# Single layer models
 class SingleLayerLinearModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -1020,19 +884,6 @@ class AnnotatedConvBnModel(torch.nn.Module):
         x = self.conv(x)
         x = self.bn(x)
         x = self.dequant(x)
-        return x
-
-class ConvBnReLUModel(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv = torch.nn.Conv2d(3, 5, 3, bias=False).to(dtype=torch.float)
-        self.bn = torch.nn.BatchNorm2d(5).to(dtype=torch.float)
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
         return x
 
 class AnnotatedConvBnReLUModel(torch.nn.Module):
@@ -1457,17 +1308,6 @@ class ModelForFusionWithBias(nn.Module):
         x = self.dequant(x)
         return x
 
-class ModelForLinearBNFusion(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc = nn.Linear(20, 10)
-        self.bn = nn.BatchNorm1d(10)
-        nn.init.uniform_(self.bn.weight)
-        nn.init.uniform_(self.bn.bias)
-
-    def forward(self, x):
-        return self.bn(self.fc(x))
-
 class DummyObserver(torch.nn.Module):
     def calculate_qparams(self):
         return 1.0, 0
@@ -1510,7 +1350,7 @@ class ResNetBase(torch.nn.Module):
         self.downsample = torch.nn.Identity()
         self.myop = nn.quantized.FloatFunctional()
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = torch.nn.Linear(inplanes, 1)
+
 
     def forward(self, x):
         out = self.conv1(x)
@@ -1520,12 +1360,7 @@ class ResNetBase(torch.nn.Module):
         out = self.myop.add(out, identity)
         out = self.relu2(out)
         out = self.avgpool(out)
-        out = torch.flatten(out, 1)
-        out = self.fc(out)
         return out
-
-    def fuse_model(self):
-        torch.quantization.fuse_modules(self, [['conv1', 'bn1', 'relu1']], inplace=True)
 
 class ModelMultipleOps(torch.nn.Module):
     def __init__(self):
@@ -1614,71 +1449,8 @@ class EmbeddingWithLinear(torch.nn.Module):
         super().__init__()
         self.emb = torch.nn.Embedding(num_embeddings=10, embedding_dim=12)
         self.fc = torch.nn.Linear(5, 5)
-        self.emb.qconfig = float_qparams_weight_only_qconfig
+        self.emb.qconfig = float_qparams_dynamic_qconfig
         self.qconfig = default_qconfig
 
     def forward(self, indices, linear_in):
         return self.emb(indices), self.fc(linear_in)
-
-class DenseTopMLP(nn.Module):
-
-    def __init__(self, dense_dim, dense_out, embedding_dim, top_out_in, top_out_out) -> None:
-        super(DenseTopMLP, self).__init__()
-
-        self.dense_mlp = nn.Sequential(
-            nn.Linear(dense_dim, dense_out),
-        )
-        self.top_mlp = nn.Sequential(
-            nn.Linear(dense_out + embedding_dim, top_out_in),
-            nn.Linear(top_out_in, top_out_out),
-        )
-
-    def forward(
-        self,
-        sparse_feature: torch.Tensor,
-        dense: torch.Tensor,
-    ) -> torch.Tensor:
-        dense_feature = self.dense_mlp(dense)
-        features = torch.cat([dense_feature] + [sparse_feature], dim=1)
-
-        out = self.top_mlp(features)
-        return out
-
-# thin wrapper around embedding bag, because tracing inside nn.Embedding
-# bag is not supported at the moment and this is top level
-class EmbBagWrapper(nn.Module):
-    def __init__(self, num_embeddings, embedding_dim):
-        super().__init__()
-        self.emb_bag = nn.EmbeddingBag(num_embeddings, embedding_dim, mode='sum')
-
-    def forward(self, indices, offsets):
-        return self.emb_bag(indices, offsets)
-
-class SparseNNModel(nn.Module):
-    _NUM_EMBEDDINGS = 10
-    _EMBEDDING_DIM = 5
-    _DENSE_DIM = 4
-    _DENSE_OUTPUT = 2
-    _TOP_OUT_IN = 2
-    _TOP_OUT_OUT = 2
-    _TOP_MLP_DIM = 1
-
-    def __init__(self) -> None:
-        super(SparseNNModel, self).__init__()
-
-        self.model_sparse = EmbBagWrapper(self._NUM_EMBEDDINGS, self._EMBEDDING_DIM)
-        self.dense_top = DenseTopMLP(
-            self._DENSE_DIM, self._DENSE_OUTPUT, self._EMBEDDING_DIM, self._TOP_OUT_IN,
-            self._TOP_OUT_OUT)
-
-    def forward(
-        self,
-        sparse_indices: torch.Tensor,
-        sparse_offsets: torch.Tensor,
-        dense: torch.Tensor,
-    ) -> torch.Tensor:
-
-        sparse_feature = self.model_sparse(sparse_indices, sparse_offsets)
-        out = self.dense_top(sparse_feature, dense)
-
-        return out
