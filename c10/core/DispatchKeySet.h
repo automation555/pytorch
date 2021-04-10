90dc0a409104f61612eff44eaf6c6f7011193ef4
automation <automation@mygamma.io>
2021-04-10 23:44:39 +0530

    commitMessage : Create RuntimeDispatchKeySet class to handle Undefined.

--- c10/core/DispatchKeySet.h
+++ c10/core/DispatchKeySet.h
@@ -3,7 +3,6 @@
 #include <c10/core/DispatchKey.h>
 #include <c10/util/llvmMathExtras.h>
 #include <c10/util/Exception.h>
-#include <c10/util/Metaprogramming.h>
 #include <ostream>
 
 namespace c10 {
@@ -62,8 +61,7 @@ public:
     }
   }
   // Test if a DispatchKey is in the set
-  bool inline has(DispatchKey t) const {
-    TORCH_INTERNAL_ASSERT_DEBUG_ONLY(t != DispatchKey::Undefined);
+  constexpr bool has(DispatchKey t) const {
     return static_cast<bool>(repr_ & DispatchKeySet(t).repr_);
   }
   // Test if DispatchKeySet is a superset of ks.
@@ -81,10 +79,6 @@ public:
   // Compute the set difference self - other
   DispatchKeySet operator-(DispatchKeySet other) const {
     return DispatchKeySet(repr_ & ~other.repr_);
-  }
-  // Compute self ^ other
-  DispatchKeySet operator^(DispatchKeySet other) const {
-    return DispatchKeySet(repr_ ^ other.repr_);
   }
   // Perform set equality
   bool operator==(DispatchKeySet other) const {
@@ -105,7 +99,7 @@ public:
   bool empty() const {
     return repr_ == 0;
   }
-  uint64_t raw_repr() { return repr_; }
+  uint64_t raw_repr() const { return repr_; }
   // Return the type id in this set with the highest priority (i.e.,
   // is the largest in the DispatchKey enum).  Intuitively, this
   // type id is the one that should handle dispatch (assuming there
@@ -126,11 +120,63 @@ private:
   constexpr DispatchKeySet(uint64_t repr) : repr_(repr) {}
   uint64_t repr_ = 0;
 
+};
+
+/* Note [RuntimeDispatchKeySet v.s. DispatchKeySet]
+ * RuntimeDispatchKeySet is an extended version of DispatchKeySet which additionally
+ * supports iterating and can include Undefined dispatch key.
+ * Here're some typical use cases:
+ * - Alias dispatch key maps to several runtime dispatch keys. We need to iterate
+ *   through the list when precomputing dispatchTable_.
+ * - Some alias dispatch keys e.g. Math/DefaultBackend, can map to Undefined dispatch
+ *   key which is not representable in a DispatchKeySet.
+ * - Undefined is included in autogradother_backends since it's also mapped to
+ *   AutogradOther.
+ *
+ * When to use RuntimeDispatchKeySet?
+ * Only use RuntimeDispatchKeySet when you need to iterate through the set or you need to
+ * check if Undefined is included. In all other use cases please either use DispatchKeySet
+ * directly or work on RuntimeDispatchKeySet::key_set() since DispatchKeySet is smaller in size.
+ * If you are thinking about adding new APIs to RuntimeDispatchKeySet other than key_set()/has(),
+ * first check whether it can be done on DispatchKeySet.
+ */
+class RuntimeDispatchKeySet final {
 public:
-  // STL iterator for DispatchKeySet. Iterates through all DispatchKeys in the
+  constexpr RuntimeDispatchKeySet()
+    : ks_(DispatchKeySet()), has_undefined_(false) {}
+  constexpr RuntimeDispatchKeySet(DispatchKey k)
+    : ks_(DispatchKeySet(k)), has_undefined_(k == DispatchKey::Undefined) {}
+  constexpr RuntimeDispatchKeySet(DispatchKeySet ks, bool has_undefined=false)
+    : ks_(ks), has_undefined_(has_undefined) {}
+  constexpr RuntimeDispatchKeySet(std::initializer_list<DispatchKey> ks)
+    : ks_(DispatchKeySet(ks)), has_undefined_(false)
+    {
+      for (DispatchKey k: ks) {
+        if (k == DispatchKey::Undefined) {
+          has_undefined_ = true;
+          break;
+        }
+      }
+    }
+
+  constexpr DispatchKeySet key_set() const {
+    return ks_;
+  }
+
+  constexpr bool has(DispatchKey k) const {
+    if (k == DispatchKey::Undefined) return has_undefined_;
+    return ks_.has(k);
+  }
+
+  constexpr RuntimeDispatchKeySet operator|(RuntimeDispatchKeySet other) const {
+    bool has_undefined = this->has(DispatchKey::Undefined) || other.has(DispatchKey::Undefined);
+    return RuntimeDispatchKeySet(this->key_set() | other.key_set(), has_undefined);
+  }
+
+public:
+  // STL iterator for RuntimeDispatchKeySet. Iterates through all DispatchKeys in the
   // set. The iterator is only invalidated by the destruction of the underlying
-  // DispatchKeySet as the iterator stores a pointer to the raw representation of
-  // the DispatchKeySet.
+  // RuntimeDispatchKeySet as the iterator stores a pointer to the DispatchKeySet object.
   class iterator {
    public:
     using self_type = iterator;
@@ -138,9 +184,9 @@ public:
     using value_type = DispatchKey;
     using difference_type = ptrdiff_t;
 
-    explicit iterator(const uint64_t *data_ptr, uint8_t i=0) : data_ptr_(data_ptr), i_(i) {
+    explicit iterator(const DispatchKeySet *data_ptr, bool has_undefined=false, uint8_t i=0) : data_ptr_(data_ptr), i_(i) {
       // Go to the first key in the set
-      ++(*this);
+      if (!has_undefined) ++(*this);
     }
 
     self_type& operator++() {
@@ -148,7 +194,7 @@ public:
 
       // Create a masked version of the set representation to ignore previous
       // keys that we've iterated through.
-      uint64_t masked_data = llvm::maskTrailingZeros<uint64_t>(i_) & *data_ptr_;
+      uint64_t masked_data = llvm::maskTrailingZeros<uint64_t>(i_) & data_ptr_->raw_repr();
       uint64_t firstKeyIndex = llvm::findFirstSet(masked_data);
 
       // If there are no keys, set to end iterator value
@@ -173,19 +219,22 @@ public:
     DispatchKey operator*() const { return static_cast<DispatchKey> (i_); }
 
    private:
-    const uint64_t *data_ptr_;
+    const DispatchKeySet *data_ptr_;
     uint8_t i_;
   };
 
  public:
   // Returns iterator to the first key in the set. If no keys are in the
   // set, then will return the end iterator.
-  iterator begin() const { return iterator(&repr_); }
+  iterator begin() const { return iterator(&ks_, has_undefined_); }
 
   // We do not need to iterate beyond NumDispatchKeys so we will treat this as
   // the end iterator. NumDispatchKeys will always be strictly less than 64.
-  iterator end() const { return iterator(&repr_, static_cast<uint8_t>(DispatchKey::NumDispatchKeys)); }
+  iterator end() const { return iterator(&ks_, has_undefined_, static_cast<uint8_t>(DispatchKey::NumDispatchKeys)); }
 
+private:
+  DispatchKeySet ks_;
+  bool has_undefined_;
 };
 
 C10_API std::string toString(DispatchKeySet);
@@ -193,32 +242,20 @@ C10_API std::ostream& operator<<(std::ostream&, DispatchKeySet);
 
 // autograd_dispatch_keyset should include all runtime autograd keys.
 // Alias key DispatchKey::Autograd maps to autograd_dispatch_keyset.
-// NB: keys in this set also get associated with CompositeImplicitAutograd
-constexpr DispatchKeySet autograd_dispatch_keyset = DispatchKeySet({
-    DispatchKey::AutogradCPU,
-    DispatchKey::AutogradCUDA,
-    DispatchKey::AutogradXLA,
-    DispatchKey::AutogradNestedTensor,
-    DispatchKey::AutogradMLC,
-    DispatchKey::AutogradXPU,
-    DispatchKey::AutogradPrivateUse1,
-    DispatchKey::AutogradPrivateUse2,
-    DispatchKey::AutogradPrivateUse3,
-    DispatchKey::AutogradOther,
+constexpr RuntimeDispatchKeySet autograd_dispatch_keyset ({
+  DispatchKey::AutogradCPU,
+  DispatchKey::AutogradCUDA,
+  DispatchKey::AutogradXLA,
+  DispatchKey::AutogradPrivateUse1,
+  DispatchKey::AutogradPrivateUse2,
+  DispatchKey::AutogradPrivateUse3,
+  DispatchKey::AutogradOther,
 });
-
-// See Note [TLS Initialization]
-constexpr DispatchKeySet default_included_set = DispatchKeySet({
-    DispatchKey::BackendSelect,
-    DispatchKey::InplaceOrView,
-});
-
-constexpr DispatchKeySet autograd_dispatch_keyset_with_InplaceOrView =
-  autograd_dispatch_keyset | DispatchKeySet(DispatchKey::InplaceOrView);
 
 // backend dispatch keys that map to DispatchKey::AutogradOther
-// NB: keys in this set also get associated with CompositeImplicitAutograd
-constexpr DispatchKeySet autogradother_backends = DispatchKeySet({
+// This includes Undefined dispatch key which is also used in runtime.
+// See Note [Undefined in dispatchTable_].
+constexpr RuntimeDispatchKeySet autogradother_backends ({
   DispatchKey::HIP,
   DispatchKey::FPGA,
   DispatchKey::MSNPU,
@@ -230,44 +267,37 @@ constexpr DispatchKeySet autogradother_backends = DispatchKeySet({
   DispatchKey::IDEEP,
   DispatchKey::QuantizedCPU,
   DispatchKey::QuantizedCUDA,
+  DispatchKey::ComplexCPU,
+  DispatchKey::ComplexCUDA,
   DispatchKey::CustomRNGKeyId,
   DispatchKey::MkldnnCPU,
   DispatchKey::SparseCPU,
   DispatchKey::SparseCUDA,
   DispatchKey::SparseHIP,
-  DispatchKey::Meta,
+  DispatchKey::Undefined,
 });
 
-// The set of dispatch keys that come after autograd
-// n.b. this relies on the fact that AutogradOther is currently the lowest Autograd key
-constexpr DispatchKeySet after_autograd_keyset = DispatchKeySet(
-        DispatchKeySet::FULL_AFTER,
-        c10::DispatchKey::AutogradOther
-);
+// backend_dispatch_keyset should include all runtime backend keys.
+// Alias key DispatchKey::DefaultBackend maps to backend_dispatch_keyset
+constexpr RuntimeDispatchKeySet backend_dispatch_keyset = RuntimeDispatchKeySet({
+  DispatchKey::CPU,
+  DispatchKey::CUDA,
+  DispatchKey::XLA,
+  DispatchKey::PrivateUse1,
+  DispatchKey::PrivateUse2,
+  DispatchKey::PrivateUse3,
+}) | autogradother_backends;
 
-// The set of dispatch keys that come after InplaceOrView
-constexpr DispatchKeySet after_InplaceOrView_keyset = DispatchKeySet(
-        DispatchKeySet::FULL_AFTER,
-        c10::DispatchKey::InplaceOrView
-);
+// math_dispatch_keyset contains all keys in backend_dispatch_keyset and autograd_dispatch_keyset
+// Alias key DispatchKey::Math maps to math_dispatch_keyset.
+constexpr RuntimeDispatchKeySet math_dispatch_keyset = backend_dispatch_keyset | autograd_dispatch_keyset;
 
-// true if t is a backend dispatch key
-C10_API bool isBackendDispatchKey(DispatchKey t);
-
-// Resolve alias dispatch key to DispatchKeySet if applicable
-C10_API DispatchKeySet getRuntimeDispatchKeySet(DispatchKey t);
+// Resolve alias dispatch key to RuntimeDispatchKeySet if applicable
+C10_API RuntimeDispatchKeySet getRuntimeDispatchKeySet(DispatchKey t);
 
 // Returns a DispatchKeySet of all backend keys mapped to Autograd dispatch key t,
-// DispatchKeySet is empty if t is not alias of DispatchKey::Autograd.
-C10_API DispatchKeySet getBackendKeySetFromAutograd(DispatchKey t);
-
-// Returns a DispatchKeySet of autograd related keys mapped to backend.
-C10_API DispatchKeySet getAutogradRelatedKeySetFromBackend(DispatchKey t);
-
-// This API exists because we have a use case for checking
-// getRuntimeDispatchKeySet(alias).has(DispatchKey::Undefined)
-// in OperatorEntry.cpp but we disallow it in has() API.
-C10_API bool isIncludedInAlias(DispatchKey k, DispatchKey alias);
+// RuntimeDispatchKeySet is empty if t is not alias of DispatchKey::Autograd.
+C10_API RuntimeDispatchKeySet getBackendKeySetFromAutograd(DispatchKey t);
 
 // Historically, every tensor only had a single DispatchKey, and it was always
 // something like CPU, and there wasn't any of this business where TLS
@@ -277,30 +307,11 @@ C10_API bool isIncludedInAlias(DispatchKey k, DispatchKey alias);
 // those cases.
 static inline DispatchKey legacyExtractDispatchKey(DispatchKeySet s) {
   // NB: If you add any extra keys that can be stored in TensorImpl on
-  // top of existing "backend" keys like CPU/CUDA, you need to add it
-  // here.  At the moment, autograd keys and InplaceOrView key need this
-  // treatment;
-  return (s - autograd_dispatch_keyset_with_InplaceOrView).highestPriorityTypeId();
+  // top of existing "normal" keys like CPU/CUDA, you need to add it
+  // here.  At the moment, RequiresGrad (replacement for Variable)
+  // is the most likely key that will need this treatment;
+  // After Autograd keys are moved from globally enabled set to TensorImpl,
+  // we should remove all Autograd keys before taking highestPriority.
+  return (s - autograd_dispatch_keyset.key_set()).highestPriorityTypeId();
 }
-
-template<class T>
-using is_not_DispatchKeySet = guts::negation<std::is_same<DispatchKeySet, T>>;
-
-// Given a function type, constructs a function_traits type that drops the first parameter
-// type if the first parameter is of type DispatchKeySet.
-// NB: DispatchKeySet is currently explicitly hidden from JIT (mainly to avoid pushing unnecessary
-// arguments on the stack - see Note [ Plumbing Keys Through the Dispatcher] for details).
-// If at any point in the future we need to expose this type to JIT, revisit the usage of this type alias.
-template <class FuncType>
-using remove_DispatchKeySet_arg_from_func = guts::make_function_traits_t<
-  typename guts::infer_function_traits_t<FuncType>::return_type,
-  typename std::conditional_t<
-    std::is_same<
-      DispatchKeySet,
-      typename guts::typelist::head_with_default_t<void, typename guts::infer_function_traits_t<FuncType>::parameter_types>
-    >::value,
-    guts::typelist::drop_if_nonempty_t<typename guts::infer_function_traits_t<FuncType>::parameter_types, 1>,
-    typename guts::infer_function_traits_t<FuncType>::parameter_types
-  >
->;
 }
