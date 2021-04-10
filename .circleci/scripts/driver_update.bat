78c3a7c6335927a18f6bb1fbddc63d898d446161
automation <automation@mygamma.io>
2021-04-11 04:46:25 +0530

    commitMessage : [DON'T MERGE] Test Windows container

--- .circleci/scripts/driver_update.bat
+++ .circleci/scripts/driver_update.bat
@@ -1,8 +1,25 @@
-set "DRIVER_DOWNLOAD_LINK=https://s3.amazonaws.com/ossci-windows/452.39-data-center-tesla-desktop-win10-64bit-international.exe"
-curl --retry 3 -kL %DRIVER_DOWNLOAD_LINK% --output 452.39-data-center-tesla-desktop-win10-64bit-international.exe
+curl --retry 3 -kL "https://s3.amazonaws.com/ossci-windows/NVIDIA_Corporation.cer" --output "NVIDIA_Corporation.cer"
 if errorlevel 1 exit /b 1
 
-start /wait 452.39-data-center-tesla-desktop-win10-64bit-international.exe -s -noreboot
+certutil -addstore "TrustedPublisher" NVIDIA_Corporation.cer
 if errorlevel 1 exit /b 1
 
-del 452.39-data-center-tesla-desktop-win10-64bit-international.exe || ver > NUL
+del NVIDIA_Corporation.cer
+if errorlevel 1 exit /b 1
+
+set "DRIVER_DOWNLOAD_LINK=https://s3.amazonaws.com/ossci-windows/451.82-tesla-desktop-winserver-2019-2016-international.exe"
+curl --retry 3 -kL %DRIVER_DOWNLOAD_LINK% --output 451.82-tesla-desktop-winserver-2019-2016-international.exe
+if errorlevel 1 exit /b 1
+
+mkdir logs
+
+start /wait 451.82-tesla-desktop-winserver-2019-2016-international.exe -s -noreboot -loglevel:6 -log:"%CD%:\logs"
+if errorlevel 1 exit /b 1
+
+type logs\LOG.setup.exe.log
+
+rmdir /s /q logs
+
+del 451.82-tesla-desktop-winserver-2019-2016-international.exe || ver > NUL
+
+if not exist "C:\Program Files\NVIDIA Corporation\NVSMI\nvidia-smi.exe" exit /b 1
