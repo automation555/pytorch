74fc7d4a4754579569599dfb520ef5fde8b33dd5
automation <automation@mygamma.io>
2021-04-10 22:01:36 +0530

    commitMessage : [quant][refactor] Moving the 'quantization/fx/utils.py' to 'quantization/utils.py

--- torch/quantization/fx/fusion_patterns.py
+++ torch/quantization/fx/fusion_patterns.py
@@ -1,29 +1,13 @@
 import torch
-from torch.fx.graph import Node
 from .pattern_utils import (
     register_fusion_pattern,
 )
-from .utils import _parent_name
-from .quantization_types import QuantizerCls
+from ..utils import _parent_name
 from ..fuser_method_mappings import get_fuser_method
-from abc import ABC, abstractmethod
-from typing import Any, Callable, Dict
 
 # ---------------------
-# Fusion Pattern Registrations
+# Fusion Patterns
 # ---------------------
-
-# Base Pattern Handler
-class FuseHandler(ABC):
-    """ Base handler class for the fusion patterns
-    """
-    def __init__(self, quantizer: QuantizerCls, node: Node):
-        pass
-
-    @abstractmethod
-    def fuse(self, quantizer: QuantizerCls, load_arg: Callable,
-             fuse_custom_config_dict: Dict[str, Any] = None) -> Node:
-        pass
 
 @register_fusion_pattern((torch.nn.ReLU, torch.nn.Conv1d))
 @register_fusion_pattern((torch.nn.ReLU, torch.nn.Conv2d))
@@ -40,28 +24,25 @@ class FuseHandler(ABC):
 @register_fusion_pattern((torch.nn.functional.relu, (torch.nn.BatchNorm1d, torch.nn.Conv1d)))
 @register_fusion_pattern((torch.nn.functional.relu, (torch.nn.BatchNorm2d, torch.nn.Conv2d)))
 @register_fusion_pattern((torch.nn.functional.relu, (torch.nn.BatchNorm3d, torch.nn.Conv3d)))
-class ConvBNReLUFusion(FuseHandler):
-    def __init__(self, quantizer: QuantizerCls, node: Node):
-        super().__init__(quantizer, node)
+class ConvBNReLUFusion():
+    def __init__(self, quantizer, node):
+        super().__init__()
         self.relu_node = None
         self.bn_node = None
         if (node.op == 'call_function' and node.target is torch.nn.functional.relu) or \
            (node.op == 'call_module' and type(quantizer.modules[node.target]) == torch.nn.ReLU):
             self.relu_node = node
-            assert isinstance(node.args[0], Node)
             node = node.args[0]
         assert node.op == 'call_module'
         if type(quantizer.modules[node.target]) in [torch.nn.BatchNorm1d, torch.nn.BatchNorm2d, torch.nn.BatchNorm3d]:
             self.bn_node = node
             self.bn = quantizer.modules[self.bn_node.target]
-            assert isinstance(node.args[0], Node)
             node = node.args[0]
         assert node.op == 'call_module'
         self.conv_node = node
         self.conv = quantizer.modules[self.conv_node.target]
 
-    def fuse(self, quantizer: QuantizerCls, load_arg: Callable,
-             fuse_custom_config_dict: Dict[str, Any] = None) -> Node:
+    def fuse(self, quantizer, load_arg, fuse_custom_config_dict=None):
         if fuse_custom_config_dict is None:
             fuse_custom_config_dict = {}
         additional_fuser_method_mapping = fuse_custom_config_dict.get("additional_fuser_method_mapping", {})
@@ -90,7 +71,7 @@ class ConvBNReLUFusion(FuseHandler):
         conv_parent_name, conv_name = _parent_name(self.conv_node.target)
         fuser_method = get_fuser_method(op_type_list, additional_fuser_method_mapping)
         if fuser_method is None:
-            raise NotImplementedError("Cannot fuse modules: {}".format(op_type_list))
+            raise NotImplementedError("Cannot fuse modules: {}".format(types))
         fused = fuser_method(*op_list)
         setattr(quantizer.modules[conv_parent_name], conv_name, fused)
 
@@ -107,18 +88,16 @@ class ConvBNReLUFusion(FuseHandler):
 @register_fusion_pattern((torch.nn.ReLU, torch.nn.BatchNorm2d))
 @register_fusion_pattern((torch.nn.functional.relu, torch.nn.BatchNorm3d))
 @register_fusion_pattern((torch.nn.ReLU, torch.nn.BatchNorm3d))
-class ModuleReLUFusion(FuseHandler):
-    def __init__(self, quantizer: QuantizerCls, node: Node):
-        super().__init__(quantizer, node)
+class ModuleReLUFusion():
+    def __init__(self, quantizer, node):
+        super().__init__()
         self.relu_node = node
-        assert isinstance(node.args[0], Node)
         node = node.args[0]
         assert node.op == 'call_module'
         self.module_node = node
         self.module = quantizer.modules[self.module_node.target]
 
-    def fuse(self, quantizer: QuantizerCls, load_arg: Callable,
-             fuse_custom_config_dict: Dict[str, Any] = None) -> Node:
+    def fuse(self, quantizer, load_arg, fuse_custom_config_dict=None):
         if fuse_custom_config_dict is None:
             fuse_custom_config_dict = {}
         additional_fuser_method_mapping = fuse_custom_config_dict.get("additional_fuser_method_mapping", {})
