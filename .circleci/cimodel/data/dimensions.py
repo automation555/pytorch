a0996b4f82757ddd51554ac47549a438ba643206
automation <automation@mygamma.io>
2021-04-10 13:19:27 +0530

    commitMessage : DO NOT MERGE, testing conda builds for py39

--- .circleci/cimodel/data/dimensions.py
+++ .circleci/cimodel/data/dimensions.py
@@ -3,22 +3,22 @@ PHASES = ["build", "test"]
 CUDA_VERSIONS = [
     "101",
     "102",
-    "111",
+    "110",
 ]
 
 ROCM_VERSIONS = [
     "3.10",
-    "4.0.1",
-    "4.1",
+    "4.0",
 ]
 
 ROCM_VERSION_LABELS = ["rocm" + v for v in ROCM_VERSIONS]
 
-GPU_VERSIONS = [None] + ["cuda" + v for v in CUDA_VERSIONS] + ROCM_VERSION_LABELS
+# GPU_VERSIONS = [None] + ["cuda" + v for v in CUDA_VERSIONS] + ROCM_VERSION_LABELS
+GPU_VERSIONS = ["cuda" + v for v in CUDA_VERSIONS]
 
 STANDARD_PYTHON_VERSIONS = [
-    "3.6",
-    "3.7",
-    "3.8",
+    # "3.6",
+    # "3.7",
+    # "3.8",
     "3.9"
 ]
