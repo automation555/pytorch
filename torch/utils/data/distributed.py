b3dee076d7a4a4e74f4671584235da80bc1fd89c
automation <automation@mygamma.io>
2021-04-11 14:52:23 +0530

    commitMessage : inline DistributedSampler's type annotations to python module

--- torch/utils/data/distributed.py
+++ torch/utils/data/distributed.py
@@ -1,15 +1,11 @@
 import math
-from typing import TypeVar, Optional, Iterator
-
+from typing import Optional, Sized
 import torch
-from . import Sampler, Dataset
+from . import Sampler
 import torch.distributed as dist
 
 
-T_co = TypeVar('T_co', covariant=True)
-
-
-class DistributedSampler(Sampler[T_co]):
+class DistributedSampler(Sampler[int]):
     r"""Sampler that restricts data loading to a subset of the dataset.
 
     It is especially useful in conjunction with
@@ -21,10 +17,10 @@ class DistributedSampler(Sampler[T_co]):
     .. note::
         Dataset is assumed to be of constant size.
 
-    Args:
-        dataset: Dataset used for sampling.
+    Arguments:
+        data_source (Dataset): Dataset used for sampling.
         num_replicas (int, optional): Number of processes participating in
-            distributed training. By default, :attr:`world_size` is retrieved from the
+            distributed training. By default, :attr:`rank` is retrieved from the
             current distributed group.
         rank (int, optional): Rank of the current process within :attr:`num_replicas`.
             By default, :attr:`rank` is retrieved from the current distributed
@@ -34,10 +30,6 @@ class DistributedSampler(Sampler[T_co]):
         seed (int, optional): random seed used to shuffle the sampler if
             :attr:`shuffle=True`. This number should be identical across all
             processes in the distributed group. Default: ``0``.
-        drop_last (bool, optional): if ``True``, then the sampler will drop the
-            tail of the data to make it evenly divisible across the number of
-            replicas. If ``False``, the sampler will add extra indices to make
-            the data evenly divisible across the replicas. Default: ``False``.
 
     .. warning::
         In distributed mode, calling the :meth:`set_epoch` method at
@@ -56,9 +48,8 @@ class DistributedSampler(Sampler[T_co]):
         ...     train(loader)
     """
 
-    def __init__(self, dataset: Dataset, num_replicas: Optional[int] = None,
-                 rank: Optional[int] = None, shuffle: bool = True,
-                 seed: int = 0, drop_last: bool = False) -> None:
+    def __init__(self, data_source: Sized, num_replicas: Optional[int] = None,
+                 rank: Optional[int] = None, shuffle: bool = True, seed: int = 0):
         if num_replicas is None:
             if not dist.is_available():
                 raise RuntimeError("Requires distributed package to be available")
@@ -67,51 +58,27 @@ class DistributedSampler(Sampler[T_co]):
             if not dist.is_available():
                 raise RuntimeError("Requires distributed package to be available")
             rank = dist.get_rank()
-        if rank >= num_replicas or rank < 0:
-            raise ValueError(
-                "Invalid rank {}, rank should be in the interval"
-                " [0, {}]".format(rank, num_replicas - 1))
-        self.dataset = dataset
+        self.data_source = data_source
         self.num_replicas = num_replicas
         self.rank = rank
         self.epoch = 0
-        self.drop_last = drop_last
-        # If the dataset length is evenly divisible by # of replicas, then there
-        # is no need to drop any data, since the dataset will be split equally.
-        if self.drop_last and len(self.dataset) % self.num_replicas != 0:  # type: ignore
-            # Split to nearest available length that is evenly divisible.
-            # This is to ensure each rank receives the same amount of data when
-            # using this Sampler.
-            self.num_samples = math.ceil(
-                # `type:ignore` is required because Dataset cannot provide a default __len__
-                # see NOTE in pytorch/torch/utils/data/sampler.py
-                (len(self.dataset) - self.num_replicas) / self.num_replicas  # type: ignore
-            )
-        else:
-            self.num_samples = math.ceil(len(self.dataset) / self.num_replicas)  # type: ignore
+        self.num_samples = int(math.ceil(len(self.data_source) * 1.0 / self.num_replicas))
         self.total_size = self.num_samples * self.num_replicas
         self.shuffle = shuffle
         self.seed = seed
 
-    def __iter__(self) -> Iterator[T_co]:
+    def __iter__(self):
         if self.shuffle:
             # deterministically shuffle based on epoch and seed
             g = torch.Generator()
             g.manual_seed(self.seed + self.epoch)
-            indices = torch.randperm(len(self.dataset), generator=g).tolist()  # type: ignore
+            indices = torch.randperm(len(self.data_source), generator=g).tolist()
         else:
-            indices = list(range(len(self.dataset)))  # type: ignore
+            indices = list(range(len(self.data_source)))
 
-        if not self.drop_last:
-            # add extra samples to make it evenly divisible
-            padding_size = self.total_size - len(indices)
-            if padding_size <= len(indices):
-                indices += indices[:padding_size]
-            else:
-                indices += (indices * math.ceil(padding_size / len(indices)))[:padding_size]
-        else:
-            # remove tail of data to make it evenly divisible.
-            indices = indices[:self.total_size]
+
+        # add extra samples to make it evenly divisible
+        indices += indices[:(self.total_size - len(indices))]
         assert len(indices) == self.total_size
 
         # subsample
@@ -129,7 +96,7 @@ class DistributedSampler(Sampler[T_co]):
         use a different random ordering for each epoch. Otherwise, the next iteration of this
         sampler will yield the same ordering.
 
-        Args:
+        Arguments:
             epoch (int): Epoch number.
         """
         self.epoch = epoch
