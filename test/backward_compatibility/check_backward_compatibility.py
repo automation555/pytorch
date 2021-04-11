27e68ad29ce0c8128f267bf7c1f87c5ff40d997d
automation <automation@mygamma.io>
2021-04-11 14:41:26 +0530

    commitMessage : Replaced whitelist/blacklist in test/backward_compatibility/check_bac…

--- test/backward_compatibility/check_backward_compatibility.py
+++ test/backward_compatibility/check_backward_compatibility.py
@@ -1,9 +1,9 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import argparse
 import datetime
 import re
 import sys
-from collections import defaultdict
-
 import torch
 from torch._C import parse_schema
 
@@ -15,80 +15,162 @@ from torch._C import parse_schema
 #   - Otherwise, pick a date that is far enough in the future that you
 #     believe you can land your diff before then.
 #
-# Allowlist entries can be removed after the date listed on them passes.
-#
-# Allowlist item format:
-# [
-#   0: function name regex
-#   1: date until which the allowlist entry is valid
-#   2: (optional) function argument regex
-# ]
-#
-# NB: function name DOES NOT include overload name!
+# allowlist entries can be removed after the date listed on them passes.
 allow_list = [
-    ("c10_experimental", datetime.date(2222, 1, 1)),
-    # Internal
-    ("static", datetime.date(9999, 1, 1)),
-    ("prim::ModuleDictIndex", datetime.date(9999, 1, 1)),
+    ('c10_experimental', datetime.date(2222, 1, 1)),
+    # We export some functions and classes for test_jit.py directly from libtorch.so,
+    # it's not important to have BC for them
+    ('_TorchScriptTesting.*', datetime.date(9999, 1, 1)),
     # Internal, profiler-specific ops
-    ("profiler::_call_end_callbacks_on_jit_fut*", datetime.date(9999, 1, 1)),
-    ("profiler::_record_function_enter", datetime.date(9999, 1, 1)),
-    ("aten::_qr_helper", datetime.date(2021, 1, 31)),
-    ("aten::fft", datetime.date(2021, 1, 31)),
-    ("aten::ifft", datetime.date(2021, 1, 31)),
-    ("aten::irfft", datetime.date(2021, 1, 31)),
-    ("aten::rfft", datetime.date(2021, 1, 31)),
-    ("aten::_lstsq_helper", datetime.date(9999, 1, 1)),
-    ("aten::_svd_helper", datetime.date(2021, 1, 31)),
-    ("aten::_syevd_helper", datetime.date(9999, 1, 1)),
-    ("aten::_cudnn_rnn_flatten_weight", datetime.date(2020, 12, 31)),
-    ("aten::_cudnn_rnn", datetime.date(2020, 12, 31)),
-    ("aten::_cudnn_rnn_backward", datetime.date(2020, 12, 31)),
-    ("aten::quantile", datetime.date(2021, 1, 31)),
-    ("aten::nanquantile", datetime.date(2021, 1, 31)),
-    ("aten::make_dual", datetime.date(2021, 2, 20)),
-    ("aten::unpack_dual", datetime.date(2021, 2, 20)),
-    ("aten::_fft_with_size", datetime.date(2021, 1, 31)),
-    ("aten::thnn_conv_depthwise2d_backward", datetime.date(2021, 1, 31)),
-    ("aten::slow_conv3d_backward", datetime.date(2021, 1, 31)),
-    ("aten::thnn_conv2d_backward", datetime.date(2021, 1, 31)),
-    ("aten::slow_conv_transpose3d_backward", datetime.date(2021, 1, 31)),
-    ("aten::slow_conv_transpose2d_backward", datetime.date(2021, 1, 31)),
-    ("aten::set_", datetime.date(2021, 1, 31)),
-    ("aten::native_layer_norm", datetime.date(2021, 1, 31)),
-    ("aten::native_layer_norm_backward", datetime.date(2021, 1, 31)),
-    ("aten::elu_backward", datetime.date(2021, 1, 31)),
-    ("aten::_multinomial_alias_setup", datetime.date(2021, 1, 31)),
-    ("aten::_multinomial_alias_draw", datetime.date(2021, 1, 31)),
-    ("prim::profile_optional", datetime.date(2021, 1, 31)),
-    ("aten::fake_quantize_per_tensor_affine_backward", datetime.date(2021, 2, 20)),
-    ("aten::fake_quantize_per_channel_affine_backward", datetime.date(2021, 2, 20)),
-    ("aten::rowwise_prune", datetime.date(9999, 1, 1)),
-    ("aten::_foreach_mul_", datetime.date(2021, 4, 2)),
-    ("aten::_foreach_addcdiv_", datetime.date(2021, 4, 2)),
-    ("aten::_foreach_div", datetime.date(2021, 4, 2)),
-    ("aten::_foreach_addcmul_", datetime.date(2021, 4, 2)),
-    ("aten::_foreach_sub", datetime.date(2021, 4, 2)),
-    ("aten::_foreach_add", datetime.date(2021, 4, 2)),
-    ("aten::_foreach_sub_", datetime.date(2021, 4, 2)),
-    ("aten::_foreach_add_", datetime.date(2021, 4, 2)),
-    ("aten::_foreach_mul", datetime.date(2021, 4, 2)),
-    ("aten::_foreach_div_", datetime.date(2021, 4, 2)),
-    ("aten::_foreach_addcdiv", datetime.date(2021, 4, 2)),
-    ("aten::_foreach_addcmul", datetime.date(2021, 4, 2)),
-    ("aten::mkldnn_linear", datetime.date(2021, 3, 2)),
-    ("aten::_mode*", datetime.date(2021, 5, 2)),
-    ("aten::linalg_multi_dot", datetime.date(2021, 3, 25)),
-    ("aten::coalesce", datetime.date(2021, 4, 15)),
-    ("aten::empty_meta", datetime.date(2021, 4, 1)),
-    ("aten::div", datetime.date(2021, 4, 28)),
-    ("aten::divide", datetime.date(2021, 4, 28)),
-    ("aten::batch_norm_backward_elemt", datetime.date(2021, 5, 1)),
-    ("aten::assert_async", datetime.date(2021, 5, 1)),
-    ("aten::cumprod_backward", datetime.date(2021, 5, 1)),
-    ("aten::_triangular_solve_helper", datetime.date(9999, 1, 1)),
-    ("aten::adaptive_avg_pool3d_backward", datetime.date(9999, 1, 1)),
+    ('profiler::_call_end_callbacks_on_jit_fut*', datetime.date(9999, 1, 1)),
+    ('profiler::_record_function_enter', datetime.date(9999, 1, 1)),
+    ('aten::append*', datetime.date(2020, 4, 15)),
+    ('aten::_min', datetime.date(2020, 9, 9)),
+    ('aten::_max', datetime.date(2020, 9, 9)),
+    ('aten::real*', datetime.date(2020, 7, 15)),
+    ('aten::imag*', datetime.date(2020, 7, 15)),
+    ('aten::quantize_per_tensor', datetime.date(2020, 4, 15)),
+    ('aten::index_put', datetime.date(2020, 4, 10)),
+    ('aten::index', datetime.date(2020, 4, 10)),
+    ('aten::_index_put_impl', datetime.date(2020, 4, 10)),
+    ('aten::index_put_', datetime.date(2020, 4, 10)),
+    ('aten::quantize_per_tensor', datetime.date(2020, 4, 15)),
+    ('aten::requires_grad_', datetime.date(2020, 4, 30)),
+    ('aten::sizes', datetime.date(2020, 4, 30)),
+    ('aten::strides', datetime.date(2020, 4, 30)),
+    ('aten::backward', datetime.date(2020, 4, 30)),
+    ('quantized::conv_prepack', datetime.date(2020, 6, 1)),
+    ('quantized::conv_unpack', datetime.date(2020, 6, 1)),
+    ('quantized::conv', datetime.date(2020, 6, 1)),
+    ('quantized::conv2d_prepack', datetime.date(2020, 6, 1)),
+    ('quantized::conv2d_unpack', datetime.date(2020, 6, 1)),
+    ('quantized::conv2d', datetime.date(2020, 6, 1)),
+    ('quantized::conv2d_relu', datetime.date(2020, 6, 1)),
+    ('quantized::conv3d_prepack', datetime.date(2020, 6, 1)),
+    ('quantized::conv3d_unpack', datetime.date(2020, 6, 1)),
+    ('quantized::conv3d', datetime.date(2020, 6, 1)),
+    ('quantized::conv3d_relu', datetime.date(2020, 5, 1)),
+    ('_quantized::conv2d_relu', datetime.date(2020, 6, 1)),
+    ('_quantized::conv2d', datetime.date(2020, 6, 1)),
+    ('aten::batch_norm_gather_stats_with_counts', datetime.date(2020, 6, 30)),
+    ('aten::quantized_lstm', datetime.date(2020, 6, 1)),
+    ('aten::quantized_gru', datetime.date(2020, 6, 1)),
+    ('quantized::make_quantized_cell_params', datetime.date(2020, 6, 1)),
+    ('quantized::make_quantized_cell_params_fp16', datetime.date(2020, 6, 1)),
+    ('quantized::make_quantized_cell_params_dynamic', datetime.date(2020, 6, 1)),
+    ('aten::len', datetime.date(2020, 6, 30)),
+    ('aten::keys', datetime.date(2020, 6, 30)),
+    ('aten::values', datetime.date(2020, 6, 30)),
+    ('aten::__getitem__', datetime.date(2020, 6, 30)),
+    ('aten::get', datetime.date(2020, 6, 30)),
+    ('aten::setdefault', datetime.date(2020, 6, 30)),
+    ('aten::Delete', datetime.date(2020, 6, 30)),
+    ('aten::pop', datetime.date(2020, 6, 30)),
+    ('aten::popitem', datetime.date(2020, 6, 30)),
+    ('aten::clear', datetime.date(2020, 6, 30)),
+    ('aten::update', datetime.date(2020, 6, 30)),
+    ('aten::items', datetime.date(2020, 6, 30)),
+    ('aten::copy', datetime.date(2020, 6, 30)),
+    ('aten::__contains__', datetime.date(2020, 6, 30)),
+    ('aten::_set_item', datetime.date(2020, 6, 30)),
+    ('aten::dict', datetime.date(2020, 6, 30)),
+    ('aten::tensor', datetime.date(2020, 6, 30)),
+    ('aten::as_tensor', datetime.date(2020, 6, 30)),
+    ('quantized::linear_unpack_fp16', datetime.date(2020, 6, 1)),
+    ('quantized::linear_unpack', datetime.date(2020, 6, 1)),
+    ('quantized::linear_prepack_fp16', datetime.date(2020, 6, 1)),
+    ('quantized::linear_prepack', datetime.date(2020, 6, 1)),
+    ('quantized::linear_dynamic_fp16', datetime.date(2020, 6, 1)),
+    ('quantized::linear_relu_dynamic', datetime.date(2020, 6, 1)),
+    ('quantized::linear_dynamic', datetime.date(2020, 6, 1)),
+    ('quantized::linear_relu', datetime.date(2020, 6, 1)),
+    ('quantized::linear', datetime.date(2020, 6, 1)),
+    ('aten::quantized_layer_norm', datetime.date(2020, 6, 30)),
+    ('aten::quantized_group_norm', datetime.date(2020, 6, 30)),
+    ('aten::quantized_instance_norm', datetime.date(2020, 6, 30)),
+    ('_aten::*', datetime.date(2020, 6, 1)),
+    ('_prim::*', datetime.date(2020, 6, 1)),
+    ('aten::eq', datetime.date(2020, 7, 30)),
+    ('aten::nq', datetime.date(2020, 6, 30)),
+    ('aten::lt', datetime.date(2020, 6, 30)),
+    ('aten::gt', datetime.date(2020, 6, 30)),
+    ('aten::le', datetime.date(2020, 6, 30)),
+    ('aten::ge', datetime.date(2020, 6, 30)),
+    ('aten::pow', datetime.date(2020, 6, 30)),
+    ('prim::min', datetime.date(2020, 6, 30)),
+    ('prim::max', datetime.date(2020, 6, 30)),
+    ('aten::view_as', datetime.date(2020, 6, 30)),
+    ('aten::reshape_as', datetime.date(2020, 6, 30)),
+    ('aten::pin_memory', datetime.date(2020, 6, 30)),
+    ('aten::reshape', datetime.date(2020, 6, 30)),
+    ('aten::detach', datetime.date(2020, 6, 30)),
+    ('aten::expand_as', datetime.date(2020, 6, 30)),
+    ('aten::flatten.*', datetime.date(2020, 6, 30)),
+    ('aten::contiguous', datetime.date(2020, 6, 30)),
+    ('aten::to_here', datetime.date(2020, 6, 30)),
+    ('aten::to_here(RRef(t) self, double timeout*)', datetime.date(2020, 6, 30)),
+    ('aten::local_value', datetime.date(2020, 6, 30)),
+    ('aten::log', datetime.date(2020, 7, 30)),
+    ('aten::__and__', datetime.date(2020, 7, 30)),
+    ('aten::__or__', datetime.date(2020, 7, 30)),
+    ('aten::__xor__', datetime.date(2020, 7, 30)),
+    ('aten::split', datetime.date(2020, 6, 30)),
+    ('aten::add', datetime.date(2020, 7, 30)),
+    ('aten::__upsample_bilinear', datetime.date(2020, 7, 30)),
+    ('aten::hash', datetime.date(2020, 7, 30)),
+    ('aten::divmod', datetime.date(2020, 7, 30)),
+    ('aten::sorted', datetime.date(2020, 7, 30)),
+    ('aten::__contains__', datetime.date(2020, 7, 30)),
+    ('aten::ne', datetime.date(2020, 7, 30)),
+    ('aten::index', datetime.date(2020, 7, 30)),
+    ('aten::isnan', datetime.date(2020, 7, 30)),
+    ('aten::pow', datetime.date(2020, 7, 30)),
+    ('aten::atan2', datetime.date(2020, 7, 30)),
+    ('aten::copy_', datetime.date(2020, 7, 30)),
+    ('aten::sort', datetime.date(2020, 7, 30)),
+    ('aten::_cudnn_init_dropout_state', datetime.date(2020, 7, 30)),
+    ('aten::sparse_coo_tensor', datetime.date(2020, 7, 30)),
+    ('aten::_sparse_coo_tensor_with_dims', datetime.date(2020, 7, 30)),
+    ('aten::_sparse_coo_tensor_with_dims_and_tensors', datetime.date(2020, 7, 30)),
+    ('aten::to', datetime.date(2020, 7, 15)),
+    ('aten::__lshift__', datetime.date(2020, 7, 30)),
+    ('aten::__rshift__', datetime.date(2020, 7, 30)),
+    ('aten::__round_to_zero_floordiv', datetime.date(2020, 7, 30)),
+    ('aten::gcd', datetime.date(2020, 7, 30)),
 ]
+
+
+# The nightly will fail to parse newly added syntax to schema declarations
+# Add new schemas that will fail the nightly here
+dont_parse_list = [
+    ('aten::eq', datetime.date(2020, 7, 30)),
+    ('aten::quantized_lstm', datetime.date(2020, 6, 1)),
+    ('aten::quantized_gru', datetime.date(2020, 6, 1)),
+    ('quantized::make_quantized_cell_params', datetime.date(2020, 6, 1)),
+    ('quantized::make_quantized_cell_params_fp16', datetime.date(2020, 6, 1)),
+    ('quantized::make_quantized_cell_params_dynamic', datetime.date(2020, 6, 1)),
+    ('quantized::conv_prepack', datetime.date(2020, 6, 1)),
+    ('quantized::conv_unpack', datetime.date(2020, 6, 1)),
+    ('quantized::conv', datetime.date(2020, 6, 1)),
+    ('quantized::conv2d_prepack', datetime.date(2020, 6, 1)),
+    ('quantized::conv2d_unpack', datetime.date(2020, 6, 1)),
+    ('quantized::conv2d', datetime.date(2020, 6, 1)),
+    ('quantized::conv2d_relu', datetime.date(2020, 6, 1)),
+    ('quantized::conv3d_prepack', datetime.date(2020, 6, 1)),
+    ('quantized::conv3d_unpack', datetime.date(2020, 6, 1)),
+    ('quantized::conv3d', datetime.date(2020, 6, 1)),
+    ('quantized::conv3d_relu', datetime.date(2020, 6, 1)),
+    ('quantized::linear_unpack_fp16', datetime.date(2020, 6, 1)),
+    ('quantized::linear_unpack', datetime.date(2020, 6, 1)),
+    ('quantized::linear_prepack_fp16', datetime.date(2020, 6, 1)),
+    ('quantized::linear_prepack', datetime.date(2020, 6, 1)),
+    ('quantized::linear_dynamic_fp16', datetime.date(2020, 6, 1)),
+    ('quantized::linear_relu_dynamic', datetime.date(2020, 6, 1)),
+    ('quantized::linear_dynamic', datetime.date(2020, 6, 1)),
+    ('quantized::linear_relu', datetime.date(2020, 6, 1)),
+    ('quantized::linear', datetime.date(2020, 6, 1)),
+]
+
 
 def allow_listed(schema, allow_list):
     for item in allow_list:
@@ -96,21 +178,8 @@ def allow_listed(schema, allow_list):
             continue
         regexp = re.compile(item[0])
         if regexp.search(schema.name):
-            if len(item) > 2:
-                # if arguments regex is present, use it
-                regexp_args = re.compile(item[2])
-                return bool(regexp_args.search(str(schema)))
             return True
     return False
-
-
-# The nightly will fail to parse newly added syntax to schema declarations
-# Add new schemas that will fail the nightly here
-dont_parse_list = [
-    ("_TorchScriptTesting.*", datetime.date(2099, 9, 17)),
-    ("test_backend", datetime.date(2099, 9, 17)),
-    ("dist_c10d", datetime.date(2021, 1, 30)),
-]
 
 
 def dont_parse(schema_line):
@@ -123,71 +192,62 @@ def dont_parse(schema_line):
     return False
 
 
-def check_bc(existing_schemas):
-    new_schemas = torch._C._jit_get_all_schemas()
-    new_schemas += torch._C._jit_get_custom_class_schemas()
-    new_schema_dict = defaultdict(list)
-    for s in new_schemas:
-        new_schema_dict[s.name].append(s)
-
+def check_bc(new_schema_dict):
+    existing_schemas = torch._C._jit_get_all_schemas()
+    existing_schemas += torch._C._jit_get_custom_class_schemas()
     is_bc = True
     broken_ops = []
     for existing_schema in existing_schemas:
         if allow_listed(existing_schema, allow_list):
-            print("schema: ", str(existing_schema), " found on allowlist, skipping")
+            print("Block list, skipping schema: ", str(existing_schema))
             continue
         print("processing existing schema: ", str(existing_schema))
-        matching_new_schemas = new_schema_dict.get(existing_schema.name, [])
+        new_schemas = new_schema_dict.get(existing_schema.name, [])
         found = False
-        for matching_new_schema in matching_new_schemas:
-            if matching_new_schema.is_backward_compatible_with(existing_schema):
+        for new_schema in new_schemas:
+            if new_schema.is_backward_compatible_with(existing_schema):
                 found = True
                 break
         if not found:
-            print(
-                "Can NOT find backward compatible schemas after changes "
-                "for schema {} from the following candidates:\n[\n{}\n]".format(
-                    str(existing_schema),
-                    "\n\t".join(str(s) for s in matching_new_schemas),
-                )
-            )
+            print('Can NOT find backward compatible schemas after changes '
+                  'for schema {} from the following candidates:\n[\n{}\n]'
+                  .format(
+                      str(existing_schema),
+                      "\n\t".join(str(s) for s in new_schemas)))
             # TODO Print out more details about why candidates don't match.
             broken_ops.append(str(existing_schema))
             is_bc = False
     if is_bc:
-        print("Found backward compatible schemas for all existing schemas")
+        print('Found backward compatible schemas for all existing schemas')
     else:
-        print(
-            "The PR is introducing backward incompatible changes to the "
-            "operator library. Please contact PyTorch team to confirm "
-            "whether this change is wanted or not. \n\nBroken ops: "
-            "[\n\t{}\n]".format("\n\t".join(broken_ops))
-        )
+        print('The PR is introducing backward incompatible changes to the '
+              'operator library. Please contact PyTorch team to confirm '
+              'whether this change is wanted or not. \n\nBroken ops: '
+              '[\n\t{}\n]'.format("\n\t".join(broken_ops)))
     return is_bc
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process some integers.")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument(
-        "--existing-schemas",
-        help="filename to load existing schemas",
+        '--new-schemas',
+        help='filename to load new schemas',
         type=str,
-        default="schemas.txt",
-    )
+        default='schemas.txt')
     args = parser.parse_args()
-    existing_schema_dict = dict()
-    slist = []
-    with open(args.existing_schemas, "r") as f:
+    new_schema_dict = dict()
+    with open(args.new_schemas, 'r') as f:
         while True:
             line = f.readline()
             if not line:
                 break
-
             if dont_parse(line.strip()):
                 print("Not parsing schema line: ", line.strip())
                 continue
             s = parse_schema(line.strip())
+            slist = new_schema_dict.get(s.name, [])
             slist.append(s)
+            new_schema_dict[s.name] = slist
 
-    if not check_bc(slist):
+    if not check_bc(new_schema_dict):
         sys.exit(1)
