ce35787097a8199f34c44778db8ac63942177de5
automation <automation@mygamma.io>
2021-04-11 11:50:41 +0530

    commitMessage : Handle fused scale and bias in fake fp16 layernorm

--- caffe2/quantization/server/fbgemm_pack_op.cc
+++ caffe2/quantization/server/fbgemm_pack_op.cc
@@ -207,30 +207,33 @@ void QuantizeConvBias(
         bias.data<int32_t>(), bias.data<int32_t>() + bias.numel());
   } else {
     const float* bdata = bias.data<float>();
-    vector<float> bdata_local;
+    vector<float> bdata_local(bias.numel());
     if (use_fp16) {
-      bdata_local.resize(bias.numel());
       fbgemm::RoundToFloat16(
-              bdata, bdata_local.data(), bias.numel(), false /* FLAGS_caffe2_fbgemm_fake_fp16_clamp */);
-      bdata = bdata_local.data();
+              bdata, bdata_local.data(), bias.numel(), false);
+    } else {
+      for (int i =0; i < bias.numel(); i++) {
+        bdata_local[i] = bdata[i];
+      }
     }
     b_quantized.resize(bias.numel());
     for (int g = 0; g < filter_qparams.size(); ++g) {
       int i_begin = g * (M / filter_qparams.size());
       int i_end = i_begin + (M / filter_qparams.size());
       for (int i = i_begin; i < i_end; ++i) {
-        if (round_to_nearest_even) {
+       if (round_to_nearest_even) {
           b_quantized[i] = fbgemm::Quantize<int32_t>(
-              bdata[i],
+              bdata_local[i],
               0,
               in_qparams.scale * filter_qparams[g].scale,
               32,
               true /* signed */);
         } else {
-          b_quantized[i] = round((1.0f / in_qparams.scale) * (1.0f / filter_qparams[g].scale) * bdata[i]);
+          b_quantized[i] = round((1.0f / in_qparams.scale) * (1.0f / filter_qparams[g].scale) * bdata_local[i]);
           b_quantized[i] = std::max(std::min(b_quantized[i], INT32_MAX), INT32_MIN);
         }
       }
+
     }
   }
 }
@@ -855,8 +858,6 @@ REGISTER_EXTERNAL_TENSOR_FUNCTIONS(
 REGISTER_EXTERNAL_TENSOR_FUNCTIONS(
     (TypeMeta::Id<Int8ConvDNNLowPPackedWeightBlob>()),
     Int8ConvDNNLowpPackedWeightBlobShapeFunctions);
-
-REGISTER_CPU_OPERATOR(Int8FCPackWeight, FullyConnectedDNNLowPPackWeightOp);
 
 REGISTER_CPU_OPERATOR_WITH_ENGINE(
     Int8FCPackWeight,
