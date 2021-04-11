9bf01f5477e82c2b09c7c040a6348e0fb21ca867
automation <automation@mygamma.io>
2021-04-11 10:20:33 +0530

    commitMessage : [Do not land] Prototype of exporting parameters as a named map

---
+++ torch/csrc/jit/mobile/export.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <torch/csrc/jit/mobile/module.h>
+
+namespace torch {
+namespace jit {
+namespace mobile {
+
+TORCH_API void _save_parameters(const Module& module, std::ostream& out);
+
+TORCH_API void _save_parameters(
+    const Module& module,
+    const std::string& filename);
+
+TORCH_API void _save_parameter_map(const Module& module, const std::string& filename);
+} // namespace mobile
+} // namespace jit
+} // namespace torch
